3c446bdb807417b5b87332ecaf594c4c4aec1621
Marius Iordan <dopyno@yahoo.com>
2022-10-12 00:23:19 +0100

    Final update

--- src/main/java/com/example/calculator/Controller.java
+++ src/main/java/com/example/calculator/Controller.java
@@ -8,26 +8,69 @@ import javafx.scene.text.Text;
 
 public class Controller {
     @FXML
-    private Text secondText;
+    private Text secondText, operatorText;
     @FXML
     private TextField mainText;
     @FXML
     private Button zeroB, oneB, twoB, threeB, fourB, fiveB, sixB, sevenB, eightB, nineB, clear, equalB;
     @FXML
-    private Button division, multiplication, subtraction, add;
-    String number = "";
+    private Button division, multiplication, subtraction, addB;
+    private String calculationType;
     private String firstNumber = "";
     private String currentNumber = "";
-    private String operation;
 
-    private void calculate(ActionEvent event){
-
+    public void clear(ActionEvent e){
+        mainText.setText("");
+        secondText.setText("");
+        operatorText.setText("");
+        firstNumber = "";
+        currentNumber = "";
     }
-    private String addNumber(String numButton){
-        number += numButton;
-        mainText.setText(number);
-        secondText.setText(number);
-        return number;
+    public void calculationSetup(String type){
+        this.calculationType = type;
+        operatorText.setText(type);
+        firstNumber = currentNumber;
+        currentNumber = "";
+        secondText.setText(firstNumber + " " + type + currentNumber);
+    }
+    @FXML
+    public void calculate(ActionEvent event){
+        int firstInt = Integer.parseInt(firstNumber);
+        int secondInt = Integer.parseInt(currentNumber);
+
+        switch (this.calculationType){
+            case "+"-> {
+                int rez = firstInt + secondInt;
+                secondText.setText(firstNumber + "+" + currentNumber + "=" + rez);
+                mainText.setText(String.valueOf(rez));
+            }
+            case "-" -> {
+                int rez = firstInt - secondInt;
+                secondText.setText(firstNumber + "-" + currentNumber + "=" + rez);
+                mainText.setText(String.valueOf(rez));
+            }
+            case "*" -> {
+                int rez = firstInt * secondInt;
+                secondText.setText(firstNumber + "*" + currentNumber + "=" + rez);
+                mainText.setText(String.valueOf(rez));
+            }
+            case "/" -> {
+                int rez;
+                if(secondInt == 0){
+                    rez = 0;
+                }else {
+                    rez = firstInt / secondInt;
+                }
+                secondText.setText(firstNumber + "/" + currentNumber + "=" + rez);
+                mainText.setText(String.valueOf(rez));
+            }
+        }
+    }
+    @FXML
+    private void addNumber(String numButton){
+        currentNumber += numButton;
+        mainText.setText(currentNumber);
+        secondText.setText(currentNumber);
     }
     public void addNr1(ActionEvent event){
         addNumber("1");
@@ -59,25 +102,20 @@ public class Controller {
     public void addNr0(ActionEvent event){
         addNumber("0");
     }
-    public double addTwoNumber(double firstNumber, double secondNumber){
-        return firstNumber + secondNumber;
+    @FXML
+    public void addNumbers(ActionEvent event){
+        calculationSetup("+");
     }
-    public double multiply(double firstNumber, double secondNumber){
-        return firstNumber * secondNumber;
+    @FXML
+    public void multiplyNumbers(ActionEvent event){
+        calculationSetup("*");
     }
-    public double divide(double firstNumber, double secondNumber){
-        try {
-        }catch (ArithmeticException e){
-            System.out.println(e);
-        }
-        return firstNumber / secondNumber;
+    @FXML
+    public void divideNumbers(ActionEvent event){
+        calculationSetup("/");
     }
-    public double subtract(double firstNumber, double secondNumber){
-        return firstNumber - secondNumber;
+    @FXML
+    public void subtractNumbers(ActionEvent event){
+        calculationSetup("-");
     }
-
-
-
-
-
 }
